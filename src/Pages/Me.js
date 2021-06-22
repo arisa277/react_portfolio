fec2d8c31564e0fd0af2c1babdaf3d9e4d5640b2
Arisa <ars.n.277@gmail.com>
2021-06-22 20:02:28 +0900

    fix navbar

--- src/Pages/Me.js
+++ src/Pages/Me.js
@@ -16,7 +16,7 @@ const Me = () => {
         </div>
         <div className="about_circles">
           
-            <div className="about_circle-1">Love to learn new things and solving problems</div>
+            <div className="about_circle-1">Love to learn new things and solving problems.</div>
             <div className="about_circle-2">
             Stayed in New Zealand for 1 year,
             </div>
@@ -24,7 +24,7 @@ const Me = () => {
             So my favorite country is New Zealand! 
             </div>
             <div className="about_circle-4">
-            Since I was 18y old, my passion is taking photos
+            Since I was 18y old, my passion is taking photos.
             </div>
             <div className="about_circle-5">
               <a
