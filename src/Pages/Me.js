1b3cdbba74c7cddce16b42f753616dac3099af17
arisa277 <ars.n.277@gmail.com>
2022-01-04 11:56:50 +0100

    add modal photo function

--- src/Pages/Me.js
+++ src/Pages/Me.js
@@ -10,8 +10,23 @@ import SecondRight from "./images/skymountain.jpg";
 import ThirdLeft from "./images/sakura.jpg";
 import ThirdMiddle from "./images/park.jpg";
 import ThirdRight from "./images/cosmos.JPG";
+import { useState } from "react";
+
 
 const Me = () => {
+  const [modal, setModal] = useState(false);
+  const [modalPhoto, setModalPhoto] = useState(undefined);
+
+  const openModalPhoto = (img) => {
+    setModal(true);
+    setModalPhoto(img)
+  }
+
+  const closeModalPhoto = () => {
+    setModal(false);
+    setModalPhoto(undefined)
+  }
+
   return (
     <div className="about_container">
       <h3 className="about_title">Me</h3>
@@ -26,17 +41,25 @@ const Me = () => {
       </div>
 
       <div className="photos">
-        <img src={FirstLeft} alt="stockholm" />
-        <img src={FirstMiddle} alt="paris" />
-        <img src={FirstRight} alt="paris-river" />
-        <img src={SecondLeft} alt="paris" />
-        <img src={SecondMiddle} alt="paris" />
-        <img src={SecondRight} alt="paris" />
-        <img src={ThirdLeft} alt="paris" />
-        <img src={ThirdMiddle} alt="paris" />
-        <img src={ThirdRight} alt="paris" />
-        <button className="more_btn"><a href="https://www.instagram.com/aripei_27">More photos <FontAwesomeIcon icon={faInstagram} size="lg" color="#fff" spin /></a></button>
+        <img src={FirstLeft} alt="stockholm" onClick={() => openModalPhoto(FirstLeft)} />
+        <img src={FirstMiddle} alt="paris" onClick={() => openModalPhoto(FirstMiddle)} />
+        <img src={FirstRight} alt="paris-river" onClick={() => openModalPhoto(FirstRight)} />
+        <img src={SecondLeft} alt="castlehill" onClick={() => openModalPhoto(SecondLeft)} />
+        <img src={SecondMiddle} alt="mtcook" onClick={() => openModalPhoto(SecondMiddle)} />
+        <img src={SecondRight} alt="mtcookwithsky" onClick={() => openModalPhoto(SecondRight)} />
+        <img src={ThirdLeft} alt="cherryblossam" onClick={() => openModalPhoto(ThirdLeft)} />
+        <img src={ThirdMiddle} alt="cornpark" onClick={() => openModalPhoto(ThirdMiddle)} />
+        <img src={ThirdRight} alt="cosmos" onClick={() => openModalPhoto(ThirdRight)} />
+        <button className="more_btn"><a href="https://www.instagram.com/aripei_27" target="_blank" rel="noreferrer">More photos <FontAwesomeIcon icon={faInstagram} size="lg" color="#fff" spin /></a></button>
       </div>
+      {/* modal */}
+      {modal &&
+        <div className="modal">
+          <div className="modal-background" onClick={closeModalPhoto}></div>
+          <div className="modal-content">
+            <img src={modalPhoto} alt="photos" />
+          </div>
+        </div>}
     </div>
   );
 };
